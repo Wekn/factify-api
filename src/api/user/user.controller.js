ef4b923975b32d37063e20911413a62fed36b384
Wekn pvt <87528684+Wekn@users.noreply.github.com>
2021-08-28 10:07:22 +0530

    new functions added

--- src/api/user/user.controller.js
+++ src/api/user/user.controller.js
@@ -1,33 +1,57 @@
-import getUsers, { registerUser } from "./user.service.js";
+import getUsers, { registerUser, singleUser } from "./user.service.js";
 
 // get all user controller
 const getAllUsers = (req, res) => {
   getUsers((err, result) => {
-    if (req.body.key == "ABC") {
-      if (err) {
-        console.log(err);
-        return;
-      } else {
-        return res.json({
-          status: true,
-          message: "All Users",
-          data: result
-        });
-      }
+    if (err) {
+      console.log(err);
+      return;
     } else {
       return res.json({
-        status: false,
-        message: "Key ABC"
+        status: true,
+        message: "All Users",
+        data: result
       });
     }
   });
 };
+export const getSingleuser=(req, res)=>{
+  // return res.json({
+  //   status: true,
+  //   uid: req.params.uid
+  // })
+    singleUser(req.params.uid, (err, result)=>{
+      if (err) {
+        console.log(err);
+        return res.json({
+          status: true,
+          message: "Failed",
+          error: err
+        });
+      } else {
+        return res.json({
+          status: true,
+          message: "Success",
+          data: result
+        });
+      }
+    })
+}
 
 export const registerNewUser = (req, res) => {
+  // return res.json({
+  //   status:true,
+  //   message: "Data comes",
+  //   data: req.body
+  // })
   registerUser(req.body, (err, result) => {
     if (err) {
       console.log(err);
-      return;
+      return res.json({
+        status: true,
+        message: "User not registered",
+        error: err
+      });
     } else {
       return res.json({
         status: true,
