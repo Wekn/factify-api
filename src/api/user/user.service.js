99c3ea38d8f29ffac219e3c41c6d8d48c1ec90e7
Wekn pvt <87528684+Wekn@users.noreply.github.com>
2021-08-28 10:06:42 +0530

    new functions added

--- src/api/user/user.service.js
+++ src/api/user/user.service.js
@@ -1,16 +1,30 @@
 import pool from "../../dbconfig/database.js";
-
+import bcrypt from 'bcrypt';
+import dateTime from 'node-datetime';
 const getUsers = (callBack) => {
-  pool.query(`select * from user`, [], (error, results, fields) => {
+  pool.query(`SELECT * from user ORDER BY register_time DESC`, [], (error, results, fields) => {
     if (error) {
       callBack(error);
     }
     return callBack(null, results);
   });
 };
-export const registerUser = (data, callBack) => {
-  pool.query('INSERT INTO `user`(`device_id`, `name`, `user_email`, `role`, `profile_pic`, `password`, `register_time`) VALUES (?,?,?,?,?,?,?)',
-    data,
+export const singleUser= (userid, callBack)=>{  
+  pool.query(`SELECT * from user WHERE sl=`+userid, [], (error, results, fields) => {
+    if (error) {
+      callBack(error);
+    }
+    return callBack(null, results);
+  });
+}
+export const registerUser = async (data, callBack) => {
+  const salt = await bcrypt.genSalt(10);
+  data.password = await bcrypt.hash(data.password, salt);
+  let date= dateTime.create();
+  let formattedDate = date.format('Y-m-d H:M:S');
+  // return callBack(null, data);
+  pool.query('INSERT INTO `user`(`device_id`, `name`, `user_email`, `role`, `password`,`register_time`) VALUES (?,?,?,?,?,?)',
+    [data.device_id, data.name, data.user_email, data.role, data.password, formattedDate],
     (error, results, fields) => {
       if (error) {
         callBack(error)
